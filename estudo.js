e9d149fe9dcb6c1e7a414252f24873b403d4ec92
nathannn007 <106194240+nathannn007@users.noreply.github.com>
2022-10-11 17:53:08 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -40,3 +40,28 @@ function total(){
     let resultado = (val * (ju/100)) + val;
     document.write("O total é de: " + resultado);
 }
+
+function soma(){
+    let n1 = document.getElementById("b1").value;
+    let n2 = document.getElementById("b2").value;
+    let n3 = document.getElementById("b3").value;
+    let n4 = document.getElementById("b4").value;
+    let r = Number(n1) + Number(n2) + Number(n3) + Number(n4);
+    document.getElementById("resultado").innerHTML = r;
+}
+function média(){
+    let n1 = document.getElementById("b1").value;
+    let n2 = document.getElementById("b2").value;
+    let n3 = document.getElementById("b3").value;
+    let n4 = document.getElementById("b4").value;
+    let r = 240 - (Number(n1) + Number(n2) + Number(n3) + Number(n4))/4;
+    document.getElementById("resultado").innerHTML = r;
+}
+function necessário(){
+    let n1 = document.getElementById("b1").value;
+    let n2 = document.getElementById("b2").value;
+    let n3 = document.getElementById("b3").value;
+    let n4 = document.getElementById("b4").value;
+    let r = 240 - (Number(n1) + Number(n2) + Number(n3) + Number(n4));
+    document.getElementById("resultado").innerHTML = r;
+}
